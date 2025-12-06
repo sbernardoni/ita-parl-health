91223fd884752f99c2530d1bc2d316d42588f685
Franz-Jan <franz.tjanssen@gmail.com>
2025-12-06 15:19:37 +0100

    Update requirements.py with additional libraries
    Added new imports for LDA, Gensim, and visualization libraries.

--- requirements.py
+++ requirements.py
@@ -10,14 +10,23 @@ from functools import partial
 from tqdm import tqdm
 from sklearn.preprocessing import normalize
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from pprint import pprint
 from sklearn.metrics.pairwise import cosine_similarity
+from gensim import models
+from gensim.models.coherencemodel import CoherenceModel
+import matplotlib.pyplot as plt
 import umap
 import hdbscan
 from bertopic import BERTopic
 import unicodedata
 from sentence_transformers import SentenceTransformer
-
+from collections import Counter
+from scipy.spatial.distance import jensenshannon
+import spacy
+import re
 #setting up the stopwords
 import nltk
 from nltk.corpus import stopwords
+
 nltk.download("stopwords")
